4568a1eddd4eca17f06e4e713dd4a51883883e68
hslnoni-tho <nagiryuh@DESKTOP-ARQK1BK.>
2025-01-10 00:57:28 +0100

    remove main.h for shell.h

--- shell.h
+++ shell.h
@@ -1,11 +1,12 @@
-#ifndef    MAIN_H
-#define MAIN_H
+#ifndef __SHELL_H__
+#define __SHELL_H__
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 
+void display_shell(void);
 int print_entry(void);
 
 #endif
